77f2f522ca9d037c8d184ac54f556ef6e02bf542
OPERE44 <peteropere3@gmail.com>
2022-02-19 19:42:21 +0300

    Todo list app

--- react_todo_list/src/App.js
+++ react_todo_list/src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Todo from "./components/Todo.js";
+import FormTodo from "./components/FormTodo.js";
+import { Card} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+  
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="todo-app">
-     <h1>The application is coming just chill bro and siz </h1>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
